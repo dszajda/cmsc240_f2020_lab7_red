dee97d6cfb167dc7bd17067e2188c12d57e1b5f3
nm7uh <nm7uh@mathcs02.richmond.edu>
2020-10-15 17:15:00 -0400

    Added tester.cpp

---
+++ tester.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std;
+
+int main(){
+
+    cout << "----------------" << endl;
+    cout << "Test Circle:" << endl;
+    cout << "----------------" << endl;
+
+
+    cout << "----------------" << std::endl;
+    cout << "Test Triangle:" << std::endl;
+    cout << "----------------" << std::endl;
+
+
+    cout << "----------------" << endl;
+    cout << "Test Rectangle:" << endl;
+    cout << "----------------" << endl;
+
+
+    return 0;
+}
