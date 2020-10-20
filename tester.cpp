fbaaaf9da5c996412728051ec705aacc7d9087a9
Matt Barnes <matt.barnes@richmond.edu>
2020-10-20 15:57:12 -0400

    final commit

--- tester.cpp
+++ tester.cpp
@@ -34,19 +34,6 @@ int main(){
     Rectangle rect = Rectangle("Rectangle1", 7.0, 8.0);
     rect.print();
     cout << "The area is: " << rect.getArea() << endl;
-    
-
-
-    
-    cout << "----------------" << endl;
-    cout << "Indivudal Tests: " << endl;
-    cout << "----------------" << endl; 
-    Shape* shapePtr = &cir;
-    shapePtr->print();
-    shapePtr = &tri;
-    shapePtr->print();
-    shapePtr = &rect;
-    shapePtr->print();
     return 0;
 
 
